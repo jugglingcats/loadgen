2b744223472245432fe75c74ae56041b2fa5d40f
Alfie Kirkpatrick <github@akirkpatrick.com>
2014-09-14 16:42:48 +0100

    ...

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/jugglingcats/loadgen/master/loadgen.properties
+wget https://raw.githubusercontent.com/jugglingcats/loadgen/master/evolok-ad3-loadgen-3.4-SNAPSHOT.jar
+
+apt-get update
+apt-get -y -q install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+
+apt-get -y -q install oracle-java7-installer
+
+java -jar evolok-ad3-loadgen-3.4-SNAPSHOT.jar -Dad.loadgen.autostart=true > loadgen.log 2>&1
