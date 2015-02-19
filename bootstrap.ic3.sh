d305072676d99a8f10d97d5909d1461414bcfffb
unknown <github@akirkpatrick.com>
2015-02-19 16:50:12 +0000

    Refactoring to include IC3 files

---
+++ bootstrap.ic3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/jugglingcats/loadgen/master/loadgen.properties
+wget https://raw.githubusercontent.com/jugglingcats/loadgen/master/evolok-ad3-loadgen-3.4-SNAPSHOT.jar
+
+apt-get update
+apt-get -y -q install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+
+apt-get -y -q install oracle-java7-installer
+
+echo "ad.node=loadgen-`hostname`" >> loadgen.properties
+echo "54.72.15.220 pmongo1" >> /etc/hosts
+echo "54.77.183.46 pmongo2" >> /etc/hosts
+
+java -jar evolok-ad3-loadgen-3.4-SNAPSHOT.jar -Dad.loadgen.autostart=true > loadgen.log 2>&1
